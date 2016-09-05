35c794d2b7f768ac4134d77c17c25fa468a6ab0c
jmuhwezi <muhwezigerald4@gmail.com>
2016-09-05 11:37:17 -0700

    commit 001 added project files

---
+++ app/src/main/java/com/basasa/e_property/Addproperty.java
@@ -0,0 +1,15 @@
+package com.basasa.e_property;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by basasa on 9/5/2016.
+ */
+public class Addproperty extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.addproperty);
+    }
+}
--- app/src/main/java/com/basasa/e_property/MainActivity.java
+++ app/src/main/java/com/basasa/e_property/MainActivity.java
@@ -1,5 +1,6 @@
 package com.basasa.e_property;
 
+import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Rect;
 import android.os.Bundle;
@@ -21,6 +22,8 @@ import android.widget.ImageView;
 import java.util.ArrayList;
 import java.util.List;
 import com.bumptech.glide.Glide;
+import com.sergiocasero.revealfab.RevealFAB;
+
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private AlbumsAdapter adapter;
@@ -52,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
        recyclerView.addItemDecoration(new GridSpacingItemDecoration(2, dpToPx(10), true));
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setAdapter(adapter);
-          prepareAlbums();
+//          prepareAlbums();
 
         try {
             Glide.with(this).load(R.drawable.cover).into((ImageView) findViewById(R.id.backdrop));
@@ -163,50 +166,50 @@ public class MainActivity extends AppCompatActivity {
     /**
      * Adding few albums for testing
      */
-    private void prepareAlbums() {
-        int[] covers = new int[]{
-                R.drawable.album1,
-                R.drawable.album2,
-                R.drawable.album3,
-                R.drawable.album4,
-                R.drawable.album5,
-                R.drawable.album6,
-                R.drawable.album7,
-                R.drawable.album8,
-                R.drawable.album9,
-                R.drawable.album10,
-                R.drawable.album11};
-
-        property a = new property("True Romance", 13, covers[0]);
-        albumList.add(a);
-
-        a = new property("Xscpae", 8, covers[1]);
-        albumList.add(a);
-
-        a = new property("Maroon 5", 11, covers[2]);
-        albumList.add(a);
-
-        a = new property("Born to Die", 12, covers[3]);
-        albumList.add(a);
-
-        a = new property("Honeymoon", 14, covers[4]);
-        albumList.add(a);
-
-        a = new property("I Need a Doctor", 1, covers[5]);
-        albumList.add(a);
-
-        a = new property("Loud", 11, covers[6]);
-        albumList.add(a);
-
-        a = new property("Legend", 14, covers[7]);
-        albumList.add(a);
-
-        a = new property("Hello", 11, covers[8]);
-        albumList.add(a);
-
-        a = new property("Greatest Hits", 17, covers[9]);
-        albumList.add(a);
-
-        adapter.notifyDataSetChanged();
+//    private void prepareAlbums() {
+//        int[] covers = new int[]{
+//                R.drawable.album1,
+//                R.drawable.album2,
+//                R.drawable.album3,
+//                R.drawable.album4,
+//                R.drawable.album5,
+//                R.drawable.album6,
+//                R.drawable.album7,
+//                R.drawable.album8,
+//                R.drawable.album9,
+//                R.drawable.album10,
+//                R.drawable.album11};
+//
+//        property a = new property("True Romance", 13, covers[0]);
+//        albumList.add(a);
+//
+//        a = new property("Xscpae", 8, covers[1]);
+//        albumList.add(a);
+//
+//        a = new property("Maroon 5", 11, covers[2]);
+//        albumList.add(a);
+//
+//        a = new property("Born to Die", 12, covers[3]);
+//        albumList.add(a);
+//
+//        a = new property("Honeymoon", 14, covers[4]);
+//        albumList.add(a);
+//
+//        a = new property("I Need a Doctor", 1, covers[5]);
+//        albumList.add(a);
+//
+//        a = new property("Loud", 11, covers[6]);
+//        albumList.add(a);
+//
+//        a = new property("Legend", 14, covers[7]);
+//        albumList.add(a);
+//
+//        a = new property("Hello", 11, covers[8]);
+//        albumList.add(a);
+//
+//        a = new property("Greatest Hits", 17, covers[9]);
+//        albumList.add(a);
+//
+//        adapter.notifyDataSetChanged();
     }
-}
+//}
